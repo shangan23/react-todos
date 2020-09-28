5b4d3692be431c6f63fb2a4743e33747a40768eb
Shankar Ganesh Jayaraman <shangan.23@gmail.com>
2020-09-28 17:30:56 +0530

    Updates

---
+++ precache-manifest.1aa7589f258c00b0b4a93ee86fd91779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab77eecd96086a45591414e19eb743fa",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "d041cf1c6941f62c17f9",
+    "url": "/react-todos/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "2f9b5693af0d76cdc95a",
+    "url": "/react-todos/static/js/2.1ba433d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todos/static/js/2.1ba433d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d041cf1c6941f62c17f9",
+    "url": "/react-todos/static/js/main.48af83a0.chunk.js"
+  },
+  {
+    "revision": "92363989f187eb484a14",
+    "url": "/react-todos/static/js/runtime-main.f50dedfc.js"
+  }
+]);
